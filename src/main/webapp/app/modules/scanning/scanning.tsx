cdd021cf4e3f6839760d15aa23e265d291066907
Daniey <lixin520gj@163.com>
2019-06-28 03:51:03 +0800

    删除一个登录的接口

--- src/main/webapp/app/modules/scanning/scanning.tsx
+++ src/main/webapp/app/modules/scanning/scanning.tsx
@@ -1,41 +1,12 @@
 import './scanning.scss';
-
 import React from 'react';
-import { connect } from 'react-redux';
-import { IRootState } from 'app/shared/reducers';
-import { getSession } from 'app/shared/reducers/authentication';
 import Pay from '../pay/pay';
 import Menu from '../menu/menu';
-
-export interface IScanningProp extends StateProps, DispatchProps {}
-
-export const ScanningType = () => window.location.hash.match(/loc/i) ? <Menu /> : <Pay />;
-
-export class Scanning extends React.Component<IScanningProp> {
-  componentDidMount() {
-    this.props.getSession();
-  }
-
-  render() {
-    return (
-      <div>
-        <ScanningType />
-      </div>
-    );
-  }
+export const ScanningType = () => (window.location.hash.match(/loc/i) ? <Menu /> : <Pay />);
+export default function Scanning() {
+  return (
+    <div>
+      <ScanningType />
+    </div>
+  );
 }
-
-const mapStateToProps = ({ authentication }: IRootState) => ({
-  account: authentication.account,
-  isAuthenticated: authentication.isAuthenticated
-});
-
-const mapDispatchToProps = { getSession };
-
-type StateProps = ReturnType<typeof mapStateToProps>;
-type DispatchProps = typeof mapDispatchToProps;
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Scanning);
