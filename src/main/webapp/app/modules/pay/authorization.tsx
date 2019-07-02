77b9661f591f045334f0568ae3310af23541164c
伍家成 <569475308@qq.com>
2019-07-02 16:34:11 +0800

    授权页面待定

---
+++ src/main/webapp/app/modules/pay/authorization.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import initStore from 'app/config/store';
+import { registerLocale } from 'app/config/translation';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+export default function complete() {
+  const store = initStore();
+  registerLocale(store);
+
+  const rootEl = document.getElementById('root');
+
+  const render = Component =>
+    ReactDOM.render(
+      <Provider store={store}>
+        <Component />
+      </Provider>,
+      rootEl
+    );
+
+  function goBack() {
+    history.go(0);
+  }
+
+  return (
+    <div>
+      <div
+        style={{
+          backgroundColor: '#fe4365',
+          height: '40px',
+          textAlign: 'center',
+          padding: '7px',
+          width: '100%',
+          position: 'fixed',
+          top: '0px',
+          zIndex: 1000
+        }}
+      >
+        <span onClick={goBack} style={{ float: 'left' }}>
+          <img
+            src="./content/images/back.png"
+            style={{
+              width: '24px',
+              height: '24px'
+            }}
+          />
+        </span>
+        <h5
+          style={{
+            color: '#fffde5',
+            marginTop: '3px',
+            fontSize: '1.05rem'
+          }}
+        >
+          支付宝授权页面
+        </h5>
+      </div>
+      <div
+        style={{
+          backgroundColor: 'rgba(255,253,229,0)',
+          height: '80px',
+          textAlign: 'center',
+          padding: '17px',
+          width: '100%',
+          position: 'fixed',
+          top: '140px',
+          zIndex: 1000
+        }}
+      >
+        <img src="./content/images/check.png" />
+        <h4>请点击下方按钮</h4>
+      </div>
+      <div
+        style={{
+          textAlign: 'center',
+          padding: '17px',
+          width: '100%',
+          position: 'fixed',
+          top: '400px',
+          zIndex: 1000
+        }}
+      >
+        <button
+          type="button"
+          style={{
+            backgroundColor: '#fe4365',
+            width: '80%',
+            border: 'none',
+            color: 'white',
+            textDecoration: 'none',
+            display: 'inline-block',
+            fontSize: '16px',
+            marginTop: '5px',
+            height: '45px',
+            borderRadius: '4px'
+          }}
+          onClick={goBack}
+        >
+          进行支付宝授权
+        </button>
+      </div>
+      <div style={{ height: '40px' }} />
+    </div>
+  );
+}
