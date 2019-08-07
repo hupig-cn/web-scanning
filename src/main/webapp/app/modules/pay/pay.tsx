3a4cc064813b6485e224d6eb0c9f157c310ff251
伍家成 <569475308@qq.com>
2019-08-07 18:53:17 +0800

    666

--- src/main/webapp/app/modules/pay/pay.tsx
+++ src/main/webapp/app/modules/pay/pay.tsx
@@ -84,22 +84,24 @@ export class Pay extends React.Component<IPayProp> {
           .then(val => {
             if (val.value.data !== '订单生成错误' && val.value.data !== '获取微信会员信息失败' && val.value.data !== '调用微信支付失败') {
               // @ts-ignore
-              let key = val.value.data.data[0];
+              // tslint:disable-next-line: no-shadowed-variable
+              const key = val.value.data.data[0];
               // @ts-ignore
               WeixinJSBridge.invoke(
                 'getBrandWCPayRequest',
                 {
-                  appId: key.appId, //公众号名称，由商户传入
-                  timeStamp: key.timeStamp, //时间戳，自1970年以来的秒数
-                  nonceStr: key.nonceStr, //随机串
+                  appId: key.appId, // 公众号名称，由商户传入
+                  timeStamp: key.timeStamp, // 时间戳，自1970年以来的秒数
+                  nonceStr: key.nonceStr, // 随机串
                   package: key.package,
-                  signType: 'MD5', //微信签名方式：
-                  paySign: key.paySign //微信签名
+                  signType: 'MD5', // 微信签名方式：
+                  paySign: key.paySign // 微信签名
                 },
+                // tslint:disable-next-line: only-arrow-functions
                 function(res) {
-                  if (res.err_msg == 'get_brand_wcpay_request:ok') {
+                  if (res.err_msg === 'get_brand_wcpay_request:ok') {
                     // 使用以上方式判断前端返回,微信团队郑重提示：
-                    //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
+                    // res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
                   }
                 }
               );
