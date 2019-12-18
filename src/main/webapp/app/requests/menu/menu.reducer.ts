2b6220d463ad69891fb15483bc7d9c498bcbe042
ruanmhui <2441485119@qq.com>
2019-12-18 09:42:24 +0800

    ？？？？？？？？

--- src/main/webapp/app/requests/menu/menu.reducer.ts
+++ src/main/webapp/app/requests/menu/menu.reducer.ts
@@ -1,6 +1,20 @@
 import axios from 'axios';
 
+export const menu = (userId : any,loc: any) => async dispatch => {
+    const result = await dispatch({
+      payload: axios.post('http://localhost:8084/services/basic/api/incomeDetails/test/loc?userId='+`${userId}`+'&loc='+`${loc}`)
+    });
+    return result;
+  };
+
   export const merchantName = (id : any) => async dispatch => {
+    const result = await dispatch({
+      payload: axios.post('http://localhost:8084/services/merchant/api/public/get/merchantNameAndData?id='+`${id}`)
+    });
+    return result;
+  };
+
+  export const ContentType = (id : any) => async dispatch => {
     const result = await dispatch({
       payload: axios.post('http://localhost:8084/services/merchant/api/public/get/merchantNameAndData?id='+`${id}`)
     });
@@ -12,10 +26,3 @@ export const merchantDishestype = (id : any) => async dispatch => {
   });
   return result;
 };
-
-  export const ContentType = (id : any) => async dispatch => {
-    const result = await dispatch({
-      payload: axios.post('http://localhost:8084/services/merchant/api/public/get/merchantNameAndData?id='+`${id}`)
-    });
-    return result;
-  };
