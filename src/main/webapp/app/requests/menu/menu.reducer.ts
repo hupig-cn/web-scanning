f0e3042776843e793c8fbf843aece28199f77c0e
ruanmhui <2441485119@qq.com>
2019-12-20 11:58:19 +0800

    接口

--- src/main/webapp/app/requests/menu/menu.reducer.ts
+++ src/main/webapp/app/requests/menu/menu.reducer.ts
@@ -27,3 +27,32 @@ export const merchantDishestype = (id: any, loc: any) => async dispatch => {
   });
   return result;
 };
+
+
+export const takingOrders = (iocId: any, num: any,merchatid: any,name: any) => async dispatch => {
+  const result = await dispatch({
+    payload: axios.post('http://localhost:8084/services/merchant/api/public/post/takingOrders?iocId=' + `${iocId}`+'&num=' + `${num}`+'&merchatid=' + `${merchatid}`+'&name=' + `${name}`)
+  });
+  return result;
+};
+
+export const takingOrdersNum = (iocId: any, merchatid: any) => async dispatch => {
+  const result = await dispatch({
+    payload: axios.post('http://localhost:8084/services/merchant/api/public/post/takingOrdersNum?iocId=' + `${iocId}`+'&merchatid=' + `${merchatid}`)
+  });
+  return result;
+};
+
+export const merchantOrders2 = (iocId: any, merchatid: any,other: any) => async dispatch => {
+  const result = await dispatch({
+    payload: axios.post('http://localhost:8084/services/merchant/api/public/post/merchantOrders2?iocId=' + `${iocId}`+'&merchatid=' + `${merchatid}`+'&other=' + `${other}`)
+  });
+  return result;
+};
+
+export const inAllOrders = (iocId: any, num: any,merchatid: any) => async dispatch => {
+  const result = await dispatch({
+    payload: axios.post('http://localhost:8084/services/merchant/api/public/post/inAllOrders?iocId=' + `${iocId}`+'&merchatid=' + `${merchatid}`)
+  });
+  return result;
+};
